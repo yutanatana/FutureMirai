c1eb850668216c7ec461edd0ee8d546d162b157c
yutanatana <yuxia@DESKTOP-GFA9H53>
2022-11-13 16:09:34 +0900

    まぁーん

--- src/main/java/Smaple.java
+++ src/main/java/Smaple.java
@@ -1,4 +1,19 @@
 
 public class Smaple {
 
+	public static void main (String[] args) {
+	 String address = "futureMirai@pornhub.com";
+	 String text = "今度一緒に遊びませんか";
+	 email(address,text);
+	}
+	public static void email (String address, String text) {
+		System.out.println(address + "に、以下のメールを送信しました");
+		System.out.println("件名：無題");
+		System.out.println("本文" + text);
+	}
+	public static void email (String title, String address, String text) {
+		System.out.println(address + "に、以下のメールを送信しました");
+		System.out.println("件名：" + title);
+		System.out.println("本文：" + text);
+	}
 }
