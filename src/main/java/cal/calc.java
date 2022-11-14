d15de2364e064c947bf2f2f7a765dc2aa6aaf0a0
yutanatana <yuxia@DESKTOP-GFA9H53>
2022-11-14 21:27:21 +0900

    at night

---
+++ src/main/java/cal/calc.java
@@ -0,0 +1,11 @@
+package cal;
+public class calc {
+
+	public static void main(String[] args) {
+		int a =  10; 
+		int b = 2;
+		int totall  = calLog.tasu(a,b);
+		int delta = calLog.hiku(a,b);  //delta means triangles
+		System.out.println("pulus" + totall+ "minus" + delta);
+	}
+}
