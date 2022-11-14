8d2275032fd5d037640de3c5060991de12556a88
yutanatana <yuxia@DESKTOP-GFA9H53>
2022-11-14 20:57:58 +0900

    夜勉強

--- src/main/java/blb/Practice1.java
+++ src/main/java/blb/Practice1.java
@@ -5,6 +5,14 @@ public class Practice1 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 double triangleArea = calTriangleArea(10.0,5.0);
+System.out.println("三角形の面積：" + triangleArea + "平方㎝");
 	}
-
+	public static double calTriangleArea(double bottom, double height) {
+		double area = (bottom*height)/2;
+		return area;
+	}
+public static double calTraiangleArea(double radius) {
+	double area = radius*radius*3.14;
+	return area;
+}
 }
---
+++ src/main/java/blb/Practice2.java
@@ -0,0 +1,10 @@
+package blb;
+
+public class Practice2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
