176a3e6d2f71de731d77e7c1cb5ee69d8beaa4df
yutanatana <yuxia@DESKTOP-GFA9H53>
2022-11-20 16:49:06 +0900

    入力機能を追加

--- src/main/java/youtubePractice/practice1.java
+++ src/main/java/youtubePractice/practice1.java
@@ -8,7 +8,19 @@ public class practice1 {
 		String age = "31";
 		int n = Integer.parseInt(age); //文字列を数値に変換
 		System.out.println("あなたは今年" + (n+1) + "歳になりますね。");
-		                                                                                                                
+		
+		int r = new java.util.Random().nextInt(100);//ランダムに数値を出力
+		System.out.println(r);
+		
+		System.out.println("please enter your name!");
+		String name = new java.util.Scanner(System.in).nextLine();
+		System.out.println("please enter your age");
+		int year = new java.util.Scanner(System.in).nextInt();
+		
+		System.out.println("welcome " + name + "," + year + "s old");
+		
+		
+		
 		
 	}
 
