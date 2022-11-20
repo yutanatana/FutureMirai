b8a8cc307d6d0d4e922bccdb946cf5fdf1c4aaa9
yutanatana <yuxia@DESKTOP-GFA9H53>
2022-11-20 16:05:26 +0900

    文字列変換　Integer.parseInt

--- src/main/java/youtubePractice/practice1.java
+++ src/main/java/youtubePractice/practice1.java
@@ -1,10 +1,15 @@
 package youtubePractice;
-
+//文字列を数値に変換する
 public class practice1 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
+		String age = "31";
+		int n = Integer.parseInt(age); //文字列を数値に変換
+		System.out.println("あなたは今年" + (n+1) + "歳になりますね。");
+		                                                                                                                
+		
 	}
 
 }
